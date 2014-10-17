683525a707f5b0bc58c7c4a7fb71f1e98a3eed15
dreamland <okerayigbolahan@yahoo.com>
2014-10-17 15:02:17 +0100

    new

---
+++ dreams.js
@@ -0,0 +1,34 @@
+<script type="text/javascript">
+
+$(document).ready(function(){
+  $(".question").click(function(){
+    var choice1=parseInt(input[name]:checked).value;
+    var choice2=parseInt(input[name]:cheched).value;
+    var choice3=parseInt(input[name]:checked).value;
+    var choice4=parseInt(input[name]:checked).value;
+    var choice5=parseInt(input[name]:checked).value;
+    var choice6=parseInt(input[name]:checked).value;
+    var choice7=parseInt(input[name]:checked).value;
+    var choice8=parseInt(input[name]:checked).value;
+    var choice9=parseInt(input[name]:checked).value;
+    var choice10=parseInt(input[name]:checked).value;
+    var total=choice1+choice2+choice3+choice4+choice5+choice6+choice7+choice8+choice9+choice10;
+ 		
+ 		for (var i = 0; i<question.length; i++) {
+        
+            if (total=40) {
+            	console.log("it's better your keep the land in your dreams")
+            };
+            else if(total=60){
+            	console.log("Stay strong,probably a place like Canada will fit your personality")
+            }
+            else if(total=80){
+            	console.log("New York City would definitely be a great start")
+            }
+            else{
+            	console.log("Stay cool in Paris")
+            }
+            };
+  });
+});
+  		
